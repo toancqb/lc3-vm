51c2c6791de71de4b267ed48c0d6589fb9d83326
Jeremy Stephens <jeremy.f.stephens@vumc.org>
2018-12-17 17:10:14 -0600

    Update code through chapter 6

--- main.c
+++ main.c
@@ -56,3 +56,125 @@ enum {
   FL_ZRO = 1 << 1, /* Z */
   FL_NEG = 1 << 2, /* N */
 };
+
+/* convert small bit number to 16-bit, keeping sign bit intact */
+uint16_t sign_extend(uint16_t x, int bit_count) {
+  if ((x >> (bit_count - 1)) & 1) {
+    x |= (0xFFFF << bit_count);
+  }
+  return x;
+}
+
+/* update condition flags based on outcome of result (negative, zero, or
+ * positive */
+void update_flags(uint16_t r) {
+  if (reg[r] == 0) {
+    reg[R_COND] = FL_ZRO;
+  }
+  else if (reg[r] >> 15) { /* a 1 in the left-most bit indicates negative */
+    reg[R_COND] = FL_NEG;
+  }
+  else {
+    reg[R_COND] = FL_POS;
+  }
+}
+
+int main(int argc, const char* argv[]) {
+  {Load Arguments, 12}
+  {Setup, 12}
+
+  /* set the PC to starting position */
+  /* 0x3000 is the default */
+  enum { PC_START = 0x3000 };
+  reg[R_PC] = PC_START;
+
+  int running = 1;
+  while (running)
+  {
+    /* FETCH */
+    uint16_t instr = mem_read(reg[R_PC]++);
+    uint16_t op = instr >> 12;
+
+    switch (op)
+    {
+      case OP_ADD:
+        {
+          /* destination register (DR) */
+          uint16_t r0 = (instr >> 9) & 0x7;
+          /* first operand (SR1) */
+          uint16_t r1 = (instr >> 6) & 0x7;
+          /* whether we are in immediate mode */
+          uint16_t imm_flag = (instr >> 5) & 0x1;
+
+          if (imm_flag) {
+            uint16_t imm5 = sign_extend(instr & 0x1F, 5);
+            reg[r0] = reg[r1] + imm5;
+          }
+          else {
+            uint16_t r2 = instr & 0x7;
+            reg[r0] = reg[r1] + reg[r2];
+          }
+
+          update_flags(r0);
+        }
+        break;
+      case OP_AND:
+        {AND, 7}
+        break;
+      case OP_NOT:
+        {NOT, 7}
+        break;
+      case OP_BR:
+        {BR, 7}
+        break;
+      case OP_JMP:
+        {JMP, 7}
+        break;
+      case OP_JSR:
+        {JSR, 7}
+        break;
+      case OP_LD:
+        {LD, 7}
+        break;
+      case OP_LDI:
+        {
+          /* destination register (DR) */
+          uint16_t r0 = (instr >> 9) & 0x7;
+
+          /* PCoffset 9 */
+          uint16_t pc_offset = sign_extend(instr & 0x1ff, 9);
+
+          /* add pc_offset to the current PC, look at that memory location to
+           * get the final address */
+          reg[r0] = mem_read(mem_read(reg[R_PC] + pc_offset));
+          update_flags(r0);
+        }
+        break;
+      case OP_LDR:
+        {LDR, 7}
+        break;
+      case OP_LEA:
+        {LEA, 7}
+        break;
+      case OP_ST:
+        {ST, 7}
+        break;
+      case OP_STI:
+        {STI, 7}
+        break;
+      case OP_STR:
+        {STR, 7}
+        break;
+      case OP_TRAP:
+        {TRAP, 8}
+        break;
+      case OP_RES:
+      case OP_RTI:
+      default:
+        {BAD OPCODE, 7}
+        break;
+    }
+  }
+
+  {Shutdown, 12}
+}
